c5047f7093003eca7d7383f3fda205b649c3975f
ivorum <ivailo.rumenov@outlook.com>
2023-04-04 19:30:33 +0300

    Some tochu-ups on the Ticket/TicketRepositoryTest.java

--- TicketSystem/src/test/java/com/diploma/TicketSystem/Ticketing/Ticket/TicketRepositoryTest.java
+++ TicketSystem/src/test/java/com/diploma/TicketSystem/Ticketing/Ticket/TicketRepositoryTest.java
@@ -2,6 +2,7 @@ package com.diploma.TicketSystem.Ticketing.Ticket;
 
 import com.diploma.TicketSystem.Ticketing.TicketType.TicketType;
 import com.diploma.TicketSystem.Ticketing.TicketType.TicketTypeBuilder;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -16,8 +17,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class TicketRepositoryTest {
     @Autowired
     private TicketRepository underTest;
+    @AfterEach
+    void tearDown(){
+        underTest.deleteAll();
+    }
     @Test
-    void itShouldCheckIfTicketfindByTicketNumber() {
+    void itShouldCheckIfTicketfindByTicketNumberDoseExists() {
         //given
         TicketType type=new
                 TicketTypeBuilder().setDiscription("Some description").setChekedTicket(true).build();
@@ -37,6 +42,30 @@ class TicketRepositoryTest {
 
         //then
         assertThat(exists.isPresent()).isTrue();
+
+    }
+
+    @Test
+    void itShouldCheckIfTicketfindByTicketNumberDoseNotExists() {
+        //given
+        TicketType type=new
+                TicketTypeBuilder().setDiscription("Some description").setChekedTicket(true).build();
+        List<TicketType> listOfTypes= new ArrayList<>();
+
+        listOfTypes.add(type);
+
+        Long ticketNumber = (long)55;
+        underTest.saveAllAndFlush(List.of(
+                new Ticket("DA",ticketNumber,listOfTypes),
+                new Ticket("DA",(long)66,listOfTypes),
+                new Ticket("DA", (long)77,listOfTypes)
+        ));
+
+        //when
+        Optional<Ticket> exists=underTest.findByTicketNumber((long) 88);
+
+        //then
+        assertThat(exists.isPresent()).isFalse();
 
     }
 }
