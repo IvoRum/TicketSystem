3d528819f1d14e738c577d604211333d326f4d9f
ivorum <ivailo.rumenov@outlook.com>
2023-04-13 15:31:58 +0300

    More work traing to make the jwt to work

---
+++ TicketSystem/src/main/java/com/diploma/ticket/system/demo/HelloRestController.java
@@ -0,0 +1,21 @@
+package com.diploma.ticket.system.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@Controller
+@RestController @RequestMapping("hello")
+public class HelloRestController {
+
+    @GetMapping("user")
+    public String helloUser() {
+        return "Hello User";
+    }
+
+    @GetMapping("admin")
+    public String helloAdmin() {
+        return "Hello Admin";
+    }
+
+}
