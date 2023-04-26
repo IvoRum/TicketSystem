9c146bd3a6a768898999e5277ed4326e5e0c8259
ivorum <ivailo.rumenov@outlook.com>
2023-04-26 10:46:25 +0300

    Article controller is now v2 after making Return requests. And made a new sql document graph-v6

--- TicketSystem/src/main/java/com/diploma/ticket/system/controler/ArticleController.java
+++ TicketSystem/src/main/java/com/diploma/ticket/system/controler/ArticleController.java
@@ -3,13 +3,15 @@ package com.diploma.ticket.system.controler;
 import com.diploma.ticket.system.entity.Article;
 import com.diploma.ticket.system.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping(path="api/v1/article")
+@RequestMapping(path="api/v2/article")
 public class ArticleController {
     private final ArticleService articleService;
     @Autowired
@@ -18,13 +20,24 @@ public class ArticleController {
     }
 
     @GetMapping
-    private List<Article> getArticle(){
-        return articleService.getArticles();
+    private ResponseEntity<List<Article>> getArticle(){
+        List<Article> response=new ArrayList<>();
+        try {
+            response=articleService.getArticles();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(response);
     }
 
     @PostMapping
-    public void registerNewArticle(@RequestBody Article article){
-        articleService.addNewArticle(article);
+    public ResponseEntity registerNewArticle(@RequestBody Article article){
+        try {
+            articleService.addNewArticle(article);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok("Article was created ");
     }
 
     @PutMapping(path="{articleId}")
--- TicketSystem/src/main/java/com/diploma/ticket/system/entity/Article.java
+++ TicketSystem/src/main/java/com/diploma/ticket/system/entity/Article.java
@@ -1,12 +1,17 @@
 package com.diploma.ticket.system.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.sql.Time;
 import java.util.Set;
 
-
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Getter
+@Setter
 @Table(name="Article")
 public class Article {
     @Id
@@ -20,91 +25,4 @@ public class Article {
     @ManyToMany
     @JoinColumn(name = "favor")
     Set<Favor> favors;
-    public Article(){}
-    public Article(String description, Time workStart, Time workEnd) {
-        this.description = description;
-        this.workStart = workStart;
-        this.workEnd = workEnd;
-    }
-
-    public Article(String name, String description, Time workStart, Time workEnd, String type, Set<Favor> favors) {
-        this.name = name;
-        this.description = description;
-        this.workStart = workStart;
-        this.workEnd = workEnd;
-        this.type = type;
-        this.favors = favors;
-    }
-
-    public Article(String name, String description, Time workStart, Time workEnd, String type) {
-        this.name = name;
-        this.description = description;
-        this.workStart = workStart;
-        this.workEnd = workEnd;
-        this.type = type;
-    }
-
-    public Article(Long id, String name, String description, Time workStart, Time workEnd, String type) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.workStart = workStart;
-        this.workEnd = workEnd;
-        this.type = type;
-    }
-
-    public Article(Long id, String description, Time workStart, Time workEnd) {
-        this.id = id;
-        this.description = description;
-        this.workStart = workStart;
-        this.workEnd = workEnd;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Time getWorkStart() {
-        return workStart;
-    }
-
-    public void setWorkStart(Time workStart) {
-        this.workStart = workStart;
-    }
-
-    public Time getWorkEnd() {
-        return workEnd;
-    }
-
-    public void setWorkEnd(Time workEnd) {
-        this.workEnd = workEnd;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
 }
