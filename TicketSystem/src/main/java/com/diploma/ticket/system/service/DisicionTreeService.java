d79ec67ef920a6b3763830e4154abd4248cee4e9
ivorum <ivailo.rumenov@outlook.com>
2023-07-08 09:04:34 +0300

    Last update before handing in the work

---
+++ TicketSystem/src/main/java/com/diploma/ticket/system/service/DisicionTreeService.java
@@ -0,0 +1,9 @@
+package com.diploma.ticket.system.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class DisicionTreeService {
+}
